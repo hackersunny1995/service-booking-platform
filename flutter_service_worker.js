2a743797d0d6b6b9a7294280301221c8f0f60f10
sunil <sknkum123@gmail.com>
2026-01-27 21:05:55 +0530

    Deploy: Fix booking success screen navigation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bfe3553c0da7f8fe7376d79038a77179",
+const RESOURCES = {"flutter_bootstrap.js": "c6fb44fa3a4ac026ff8bff35b1227254",
 "version.json": "407bbd149dedabfdaef7a84ce30c8cb5",
 "index.html": "b0f503e6d4cc71e529500a210e2cae84",
 "/": "b0f503e6d4cc71e529500a210e2cae84",
-"main.dart.js": "797234e138d93db35cd1444ec2b69a0b",
+"main.dart.js": "469e52655593ccc7eb5685e12b18d6d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "bfe3553c0da7f8fe7376d79038a77179",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/fonts/MaterialIcons-Regular.otf": "8c755f1329fab78db59182a363338c9c",
+"assets/fonts/MaterialIcons-Regular.otf": "91f56fa99c9e94dee9b65931421f4ebb",
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
 "canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
